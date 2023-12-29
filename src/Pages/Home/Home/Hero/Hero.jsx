371f8d3f369dd3e2197a37467a40d6512699823a
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2023-12-29 23:56:45 +0600

    update on button

--- src/Pages/Home/Home/Hero/Hero.jsx
+++ src/Pages/Home/Home/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         className="w-full h-auto object-cover"
         style={{ maxHeight: "450px" }}
       />
-      <div className="  absolute    bottom-1 right-1 ">
+      <div className="  absolute    bottom-0 right-0 ">
         <a href={resume} download={resume}>
           <button className=" block md:hidden btn btn-outline btn-sm    "> 
             <span className="text-black text-xs  font-serif ">
